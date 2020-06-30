c00cda01759ecc9e568af78c6c0f64419c10143c
Rollcloud <rollcloud@gmail.com>
2020-06-30 13:35:57 +0200

    effects.py: increase variability of lightning

--- app/effects.py
+++ app/effects.py
@@ -1,99 +1,60 @@
+import random
 import time
-
-from random import randrange
 
 from colours import Colour
 
 
 def lightning_flash(arduino, sky_colour=Colour(0, 0, 0), leds=range(60)):
-    # Excerpt of original Arduino program from Storm_Cloud below:
+    # Based on the Storm_Cloud Arduino sketch:
 
-    # const int lightPin = 3;
+    flash_sequences = (
+        [  # RGB
+            Colour(255, 255, 255),  # white
+            sky_colour,  # off
+            Colour(100, 100, 150),  # slight blue
+            sky_colour,  # off
+            Colour(50, 50, 50),  # off
+            sky_colour,  # off
+            Colour(255, 255, 255),  # white
+            sky_colour,  # off
+            sky_colour,  # off
+            sky_colour,  # off
+            Colour(255, 120, 255),  # purple
+        ],
+        [
+            Colour(100, 100, 150),  # slight blue
+            sky_colour,  # off
+            Colour(255, 255, 255),  # white
+            sky_colour,  # off
+            sky_colour,  # off
+            Colour(255, 120, 255),  # purple
+            sky_colour,  # off
+            sky_colour,  # off
+            Colour(255, 255, 255),  # white
+            sky_colour,  # off
+            Colour(50, 50, 50),  # off
+        ],
+        [
+            Colour(255, 200, 120),  # orange
+            sky_colour,  # off
+            sky_colour,  # off
+            sky_colour,  # off
+            Colour(255, 255, 255),  # white
+            sky_colour,  # off
+            Colour(255, 120, 255),  # purple
+            sky_colour,  # off
+            Colour(50, 50, 50),  # off
+            sky_colour,  # off
+            Colour(200, 150, 100),  # slight orange
+        ],
+    )
 
-    # int rainCloud [25] [3] = {
-    #  {256,256,256}, //white
-    #  {0,0,0}, // off
-    #  {100,100,150}, // slight blue
-    #  {0,0,0}, // off
-    #  {50,50,50}, // off
-    #  {0,0,0}, // off
-    #  {256,256,256}, //white
-    #  {0,0,0}, // off
-    #  {0,0,0}, // off
-    #  {0,0,0}, // off
-    #  {255,120,255}, // purple
-    #  {0,0,0}, // off
-    #  {0,0,0}, // off
-    #  {100,100,150}, // slight blue
-    #  {0,0,0}, // off
-    #  {256,256,256}, //white
-    #  {0,0,0}, // off
-    #  {0,0,0}, // off
-    #  {0,0,0}, // off
-    #  {255,120,255}, // purple
-    #  {0,0,0}, // off
-    #  {0,0,0}, // off
-    #  {256,256,256}, //white
-    #  {50,50,50}, // off
-    #  {0,0,0}, // off
-    # };
-
-    # void loop() {
-    #   if(digitalRead(lightPin) == LOW) {
-    #     for(int i=0; i< 10; i++) {
-    #       runLightning();
-    #     }
-    #     for(int j=255,
-    #   }
-    #   delay(5);
-    # }
-
-    # void runLightning() {
-    #     for(int i=0; i< 25; i++) {
-    #     setRGB(rainCloud[i][0],rainCloud[i][1],rainCloud[i][2]);
-    #     delay(random(30,250));
-    #     if(i == 12) delay(random(250,1000));
-    #   }
-    #   delay(random(1000,5000));
-    # }
-
-    flash_sequence = [  # RGB
-        Colour(255, 255, 255),  # white
-        sky_colour,  # off
-        Colour(100, 100, 150),  # slight blue
-        sky_colour,  # off
-        Colour(50, 50, 50),  # off
-        sky_colour,  # off
-        Colour(255, 255, 255),  # white
-        sky_colour,  # off
-        sky_colour,  # off
-        sky_colour,  # off
-        Colour(255, 120, 255),  # purple
-        sky_colour,  # off
-        sky_colour,  # off
-        Colour(100, 100, 150),  # slight blue
-        sky_colour,  # off
-        Colour(255, 255, 255),  # white
-        sky_colour,  # off
-        sky_colour,  # off
-        sky_colour,  # off
-        Colour(255, 120, 255),  # purple
-        sky_colour,  # off
-        sky_colour,  # off
-        Colour(255, 255, 255),  # white
-        Colour(50, 50, 50),  # off
-        sky_colour,  # off
-    ]
-
-    for i, each in enumerate(flash_sequence):
+    for i, each in enumerate(random.choice(flash_sequences)):
         arduino.send_solid_range(each, leds)
+        time.sleep(random.randrange(5, 100) / 1000)
 
-        time.sleep(randrange(30, 250) / 1000)
-        if i == 12:
-            time.sleep(randrange(250, 1000) / 1000)
-
-    # add delay of 1 sec - 5 sec before repeating
-    # time.sleep(randrange(1, 5))
+    # restore sky to original colour
+    arduino.send_solid_range(sky_colour, leds)
 
 
 def main():
